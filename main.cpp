e089ccad44dbb836464fbb8a97287b1b76bcca5d
Luis Lüttgens <luis1@uni-bremen.de>
2018-12-16 12:13:11 +0100

    first commit includes some soluiton for leetcode challenges

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "playground/leetCodeImplementation.h"
+
+int main() {
+    std::vector<int> a {1,4,2,3};
+    leetCodeImplementation::arrayPairSum(a);
+
+    leetCodeImplementation::selfDividingNumbers(1,10);
+}
